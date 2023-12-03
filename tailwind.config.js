7a82b5c9384734330e636f95a1c2b2ac0e06fed2
unknown <matheusfariasdasneves@gmail.com>
2023-12-03 19:53:13 -0300

    feat: adicionando variaveis para as cores que serao usadas no design do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#301951',
+        secondary: '#1F142F',
+        offWhite: '#E7E7E7',
+        placeholder: '#BBADCF',
+        whiteColor: '#FFF',
+        greenColor: '#1FC61B',
+        greyColor: '#4B4B4B',
+        redColor: '#C61B1B'
+      },
+    },
   },
   plugins: [],
 }
