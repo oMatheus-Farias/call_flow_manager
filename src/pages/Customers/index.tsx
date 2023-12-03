5172ee69cb59f8230bfb5328ebf397187429ec6a
unknown <matheusfariasdasneves@gmail.com>
2023-12-03 15:21:20 -0300

    feat: criado os arquivos das paginas do projeto

---
+++ src/pages/Customers/index.tsx
@@ -0,0 +1,7 @@
+export default function Customers(){
+  return(
+    <div>
+      Page Customers
+    </div>
+  );
+};
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,7 @@
+export default function Profile(){
+  return(
+    <div>
+      Page Profile
+    </div>
+  );
+};
