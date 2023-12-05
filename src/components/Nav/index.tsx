30f86b77d0bd158245d88eeaff340ec6bfbdf93b
unknown <matheusfariasdasneves@gmail.com>
2023-12-05 18:12:32 -0300

    feat: adicionada renderizaçao condicional para foto do usuario

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
+
 import avatarImgNull from "../../assets/avatar.png";
 import { Link } from "react-router-dom";
 
 export default function Nav(){
+  const { user } = useContext(AuthContext);
+
   return(
     <nav className="w-full h-14 bg-gradient-to-t from-secondary to-primary flex md:max-w-[16.2em] md:h-full md:rounded-r-[2.5em] md:flex md:flex-col md:items-center" >
       <div className="flex flex-col items-center justify-center" >
         <img
           className="hidden md:block object-cover max-w-[6.4em] max-h-[6.4em] rounded-full border-4 border-white mt-4 mb-16"
-          src={ avatarImgNull }
+          src={ !user?.avatarUrl ? avatarImgNull : user.avatarUrl }
           alt="Foto do usuario"
         />
       </div>
@@ -16,7 +21,7 @@ export default function Nav(){
         <li 
           className="text-white hover:bg-hoverColor transition-all flex items-center md:text-xl md:w-full md:justify-center md:py-2" >
           <Link className="md:flex md:items-center md:gap-2 md:w-[50%]" to='/dashboard' >
-            <svg className="hidden md:block text-white h-6 w-6"  fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+            <svg className="hidden md:block text-white h-6 w-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
               <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z"/>
             </svg>
             Chamados
@@ -42,45 +47,5 @@ export default function Nav(){
         </li>
       </ul>
     </nav>
-
-    // <nav className="max-w-[16.2em] h-full bg-gradient-to-t from-secondary to-primary rounded-r-[2.5em] flex flex-col items-center" >
-    //   <div className="flex flex-col items-center justify-center" >
-    //     <img
-    //       className="object-cover max-w-[6.4em] max-h-[6.4em] rounded-full border-4 border-white mt-4 mb-16"
-    //       src={ avatarImgNull }
-    //       alt="Foto do usuario"
-    //     />
-    //   </div>
-
-    //   <ul className="flex flex-col gap-5 w-full" >
-    //     <li 
-    //       className="text-xl text-white w-full flex justify-center py-2 hover:bg-hoverColor transition-all" >
-    //       <Link className="flex items-center gap-2 w-[50%]" to='/dashboard' >
-    //         <svg className="text-white h-6 w-6"  fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-    //           <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z"/>
-    //         </svg>
-    //         Chamados
-    //       </Link>
-    //     </li>
-    //     <li 
-    //       className="text-xl text-white w-full flex justify-center py-2 hover:bg-hoverColor transition-all" >
-    //       <Link className="flex items-center gap-2 w-[50%]" to='/customers' >
-    //       <svg  className="text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="16" width="20" viewBox="0 0 640 512">
-    //         <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/>
-    //       </svg>
-    //         Clientes
-    //       </Link>
-    //     </li>
-        // <li 
-        //   className="text-xl text-white w-full flex justify-center py-2 hover:bg-hoverColor transition-all" >
-        //   <Link className="flex items-center gap-2 w-[50%]" to='/profile' >
-        //   <svg className="text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="16" width="16" viewBox="0 0 512 512">
-        //     <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/>
-        //   </svg>
-        //     Perfil
-        //   </Link>
-        // </li>
-    //   </ul>
-    // </nav>
   );
 };
