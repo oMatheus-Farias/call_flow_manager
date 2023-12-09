06360a32687b2c3a44489f461109150198fc2e76
unknown <matheusfariasdasneves@gmail.com>
2023-12-08 23:43:17 -0300

    feat: retirado o efeito de hover na navegaçao em telas menores

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -19,7 +19,7 @@ export default function Nav(){
 
       <ul className="flex justify-around w-full md:flex-col md:gap-5">
         <li 
-          className="text-white hover:bg-hoverColor transition-all flex items-center md:text-xl md:w-full md:justify-center md:py-2" >
+          className="text-white transition-all flex items-center md:text-xl md:w-full md:justify-center md:py-2 md:hover:bg-hoverColor" >
           <Link className="md:flex md:items-center md:gap-2 md:w-[50%]" to='/dashboard' >
             <svg className="hidden md:block text-white h-6 w-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
               <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z"/>
@@ -28,7 +28,7 @@ export default function Nav(){
           </Link>
         </li>
         <li 
-          className="text-white hover:bg-hoverColor transition-all flex items-center md:text-xl md:w-full md:justify-center md:py-2" >
+          className="text-white  transition-all flex items-center md:text-xl md:w-full md:justify-center md:py-2 md:hover:bg-hoverColor" >
           <Link className="md:flex md:items-center md:gap-2 md:w-[50%]" to='/customers' >
           <svg  className="hidden md:block text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="16" width="20" viewBox="0 0 640 512">
             <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/>
@@ -37,7 +37,7 @@ export default function Nav(){
           </Link>
         </li>
         <li 
-          className="text-white hover:bg-hoverColor transition-all flex items-center md:text-xl md:w-full md:justify-center md:py-2" >
+          className="text-white  transition-all flex items-center md:text-xl md:w-full md:justify-center md:py-2 md:hover:bg-hoverColor" >
           <Link className="md:flex md:items-center md:gap-2 md:w-[50%]" to='/profile' >
           <svg className="hidden md:block text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="16" width="16" viewBox="0 0 512 512">
             <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/>
